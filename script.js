d02507feebfbbab15f821be9ff82d20ba1b7b599
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2024-07-28 12:57:12 -0400

    Smooth out transition for sections

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-const collapse = document.getElementsByClassName('collapse');
-let i;
-
-for (i = 0; i < collapse.length; i++) {
-  collapse[i].addEventListener('click', function () {
-    this.classList.toggle('active');
-    let content = this.nextElementSibling;
-    if (content.style.maxHeight) {
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + 'px';
-    }
-  });
-}
+document.addEventListener('DOMContentLoaded', function () {
+  const collapse = document.getElementsByClassName('collapse');
+  for (let i = 0; i < collapse.length; i++) {
+    collapse[i].addEventListener('click', function () {
+      this.classList.toggle('active');
+      let content = this.nextElementSibling;
+      if (content.style.display === 'block') {
+        content.style.display = 'none';
+        content.style.height = '0px';
+      } else {
+        content.style.display = 'block';
+        content.style.height = content.scrollHeight + 'px';
+      }
+    });
+  }
+});
