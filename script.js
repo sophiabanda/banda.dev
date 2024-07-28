d095a695f4cb2ca21a49ada6156e2312844801fc
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2024-07-28 12:10:23 -0400

    Add shadow to header text to imporove readability

---
+++ script.js
@@ -0,0 +1,14 @@
+const collapse = document.getElementsByClassName('collapse');
+let i;
+
+for (i = 0; i < collapse.length; i++) {
+  collapse[i].addEventListener('click', function () {
+    this.classList.toggle('active');
+    let content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + 'px';
+    }
+  });
+}
