a49f0e7fc6539c61a12bf8565943dad88f3afa8f
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2024-08-01 11:06:18 -0400

    WIP overhaul of html structure

--- script.js
+++ script.js
@@ -12,26 +12,3 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 });
-
-// window.addEventListener(
-//   'mousemove',
-//   function (e) {
-//     const arr = [1, 0.9, 0.8, 0.5, 0.2];
-
-//     arr.forEach(function (i) {
-//       var x = (1 - i) * 75;
-//       var star = document.createElement('div');
-
-//       star.className = 'star';
-//       star.style.top = e.pageY + Math.round(Math.random() * x - x / 2) + 'px';
-//       star.style.left = e.pageX + Math.round(Math.random() * x - x / 2) + 'px';
-
-//       document.body.appendChild(star);
-
-//       window.setTimeout(function () {
-//         document.body.removeChild(star);
-//       }, Math.round(Math.random() * i * 600));
-//     });
-//   },
-//   false
-// );
