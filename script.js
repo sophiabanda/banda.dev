d9500ab952c985a048f49f559125c710500e2001
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2024-08-02 19:49:16 -0400

    Add back stars, remove thought comments

--- script.js
+++ script.js
@@ -9,20 +9,16 @@ document.addEventListener('DOMContentLoaded', function () {
       const targetId = this.getAttribute('data-target');
       const targetContent = document.getElementById(targetId).innerHTML;
 
-      // Clear the modal body before appending new content
       modalBody.innerHTML = '';
 
-      // Load the targeted content into the modal body
       modalBody.innerHTML = targetContent;
 
-      // Apply styles to the work section if it exists
       if (targetId === 'work') {
         modalBody.classList.add('scrollable-section');
       } else {
         modalBody.classList.remove('scrollable-section');
       }
 
-      // Only show the modal if there is content to display
       if (modalBody.innerHTML.trim() !== '') {
         modal.style.display = 'flex';
       }
@@ -40,25 +36,25 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
-// window.addEventListener(
-//   'mousemove',
-//   function (e) {
-//     const arr = [1, 0.9, 0.8, 0.5, 0.2];
+window.addEventListener(
+  'mousemove',
+  function (e) {
+    const arr = [1, 0.9, 0.8, 0.5, 0.2];
 
-//     arr.forEach(function (i) {
-//       var x = (1 - i) * 75;
-//       var star = document.createElement('div');
+    arr.forEach(function (i) {
+      var x = (1 - i) * 75;
+      var star = document.createElement('div');
 
-//       star.className = 'star';
-//       star.style.top = e.pageY + Math.round(Math.random() * x - x / 2) + 'px';
-//       star.style.left = e.pageX + Math.round(Math.random() * x - x / 2) + 'px';
+      star.className = 'star';
+      star.style.top = e.pageY + Math.round(Math.random() * x - x / 2) + 'px';
+      star.style.left = e.pageX + Math.round(Math.random() * x - x / 2) + 'px';
 
-//       document.body.appendChild(star);
+      document.body.appendChild(star);
 
-//       window.setTimeout(function () {
-//         document.body.removeChild(star);
-//       }, Math.round(Math.random() * i * 600));
-//     });
-//   },
-//   false
-// );
+      window.setTimeout(function () {
+        document.body.removeChild(star);
+      }, Math.round(Math.random() * i * 600));
+    });
+  },
+  false
+);
